db63ac5b736c224ec787e73c19bb72c8d56ce887
Builderbot2000 <skenshin2000@outlook.com>
2025-02-25 23:31:49 -0800

    data assess conversion

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,22 @@
 import unittest
+import json
 from app.app import lambda_handler
-
 
 class TestApp(unittest.TestCase):
     def test_app(self):
-        event = {"max_time_window": 2, "ticker_symbol": "AAPL", "interval": "1h"}
+        with open('./tests/assessment_test_data.json') as f:
+            test_data = json.load(f)
+        
+        event = {
+            "max_time_window": 2,
+            "ticker_symbol": "AAPL",
+            "interval": "1h",
+            "test_data": test_data
+        }
         context = {}
         response = lambda_handler(event, context)
         self.assertEqual(response["statusCode"], 200)
-        self.assertIn("Hello from Lambda!", response["body"])
-        self.assertIn("AAPL", response["body"])
-        self.assertIn("2", response["body"])
-
+        self.assertIn("volatility", response["body"])
 
 if __name__ == "__main__":
     unittest.main()
